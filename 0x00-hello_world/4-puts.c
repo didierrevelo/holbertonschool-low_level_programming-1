8e5d1f658ae6e7afbac9ba6de80e49a9142eb7ca
didierrevelo <1829@holbertonschool.com>
2020-06-11 00:26:03 +0000

    cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print with pats
  *
+ * description: this function allows me to print a statement without using printf.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
