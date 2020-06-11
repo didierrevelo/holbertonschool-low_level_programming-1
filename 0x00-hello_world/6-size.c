5e512aae4a6b2164140db4c0fc8d627fcdd1c195
didierrevelo <1829@holbertonschool.com>
2020-06-11 02:48:50 +0000

    cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %ld byte\n", (long int) sizeof(c));
-printf("Size of an int: %ld bytes\n", (long int) sizeof(i));
-printf("Size of a long int: %ld bytes\n", (long int) sizeof(li));
-printf("Size of a long long int: %ld bytes\n", (long int) sizeof(lli));
-printf("Size of float: %ld bytes\n", (long int) sizeof(f));
+printf("Size of a char: %ld byte(s)\n", (long int) sizeof(c));
+printf("Size of an int: %ld byte(s)\n", (long int) sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", (long int) sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", (long int) sizeof(lli));
+printf("Size of float: %ld byte(s)\n", (long int) sizeof(f));
 return (0);
 }
