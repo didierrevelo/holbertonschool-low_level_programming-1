3dddf5cc154f12303877d6a388245184c9645f33
didierrevelo <1829@holbertonschool.com>
2020-06-13 00:06:36 -0500

    cisfun

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,11 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("last digit of %i is %i ", n, n % 10);
 if ((n % 10) > 5)
-printf("and is greater than 5\n");
+printf("last digit of %d is %d and is 0\n", n, n % 10);
 else if ((n % 10) == 0)
-printf("and is 0\n");
+printf("last digit of %d is %d and is and greater than 5\n", n, n % 10);
 else
-printf("and is less than 6 and not 0\n");
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 return (0);
 }
