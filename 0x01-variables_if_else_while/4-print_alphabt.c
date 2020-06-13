4296ac80da973eccd3a5c744834a68677dfcc442
didierrevelo <1829@holbertonschool.com>
2020-06-13 18:09:05 -0500

    cisfun

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char X = 'a';
+
+	for (X = 'a'; X <= 'z'; X++)
+		if (X != 'e')
+			if (X != 'q')
+				putchar(X);
+	putchar('\n');
+	return (0);
+}
