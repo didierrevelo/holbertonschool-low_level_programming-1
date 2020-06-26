4706c05c087bf4773d4d70d44664e71d9b14745c
didierrevelo <1829@holbertonschool.com>
2020-06-26 14:19:52 -0500

    more

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
-
+/**
+ **cap_string - this function capitalizes all words of a string.
+ *@a: integer
+ *Return: 0
+ */
 char *cap_string(char *a)
 {
 	int x;
-	x = 0;
 
 	while (a[x] != '\0')
 	{
@@ -11,12 +14,12 @@ char *cap_string(char *a)
 		{
 			a[0] -= 32;
 		}
-		if (a[x] >= 97 && a[x] <= 122 && (a[x -1] == ',' || a[x - 1] == ';'
-			    || a[x -1] == '.' || a[x -1] == '!' || a[x -1] == '?'
-			    || a[x -1] == '"' || a[x -1] == '\n'
-			    || a[x -1] == '(' || a[x -1] == ')'
-			    || a[x -1] == '{' || a[x -1] == '}'
-			    || a[x -1] == ' ' || a[x -1] == 32 || a[x -1] == '\t'))
+		if (a[x] >= 97 && a[x] <= 122 && (a[x - 1] == ',' || a[x - 1] == ';'
+			    || a[x - 1] == '.' || a[x - 1] == '!' || a[x - 1] == '?'
+			    || a[x - 1] == '"' || a[x - 1] == '\n'
+			    || a[x - 1] == '(' || a[x - 1] == ')'
+			    || a[x - 1] == '{' || a[x - 1] == '}'
+			    || a[x - 1] == ' ' || a[x - 1] == 32 || a[x - 1] == '\t'))
 		{
 			a[x] -= 32;
 		}
