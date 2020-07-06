418b8adf8ae6dad1370dadcf08459a1832729c64
didierrevelo <1829@holbertonschool.com>
2020-07-05 23:30:52 -0500

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
-int main (int argc, char * argv [])
+/**
+ *main - program that prints its name, followed by a new line.
+ *@argc: number of commant line
+ *@argv: command line input
+ *Return: the name of program
+ */
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf ("%s" "\n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
