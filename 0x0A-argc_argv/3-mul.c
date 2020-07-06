6a2571329593f44f65d77f8b6fc6115301b17aae
didierrevelo <1829@holbertonschool.com>
2020-07-06 00:58:33 -0500

    arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *main - program that multiplies two numbers.
+ *@argc: number of commant line
+ *@argv: command line input
+ *Return: if the program does not receive two arguments, your program should
+ *print Error, followed by a new line, and return 1.
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	else
+		mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+
+	return (0);
+}
