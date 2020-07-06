2da4b6907876ef50181031d2ccaaff92cacc9ba3
didierrevelo <1829@holbertonschool.com>
2020-07-05 23:56:56 -0500

    arg

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,14 +11,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("%s\n", argv[0]);
-
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			printf(" %s\n", argv[i]);
-		}
+		printf(" %s\n", argv[i]);
 	}
 
 	return (0);
