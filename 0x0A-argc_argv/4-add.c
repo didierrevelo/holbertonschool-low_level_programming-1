f69aa0c3831bec024a4cfc452e127f33e3fe3922
didierrevelo <1829@holbertonschool.com>
2020-07-06 22:40:44 -0500

    arg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ *_isdigit - this function checks for a digit
+ *@c: input
+ *Return: 1 if c is a digit or 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ *main - program that adds positive numbers.
+ *@argc: number of commant line
+ *@argv: command line input
+ *Return: If no number is passed to the program, print 0, followed by a new line
+ * If one of the number contains symbols that are not digits, print Error,
+ * followed by a new line, and return 1
+ */
+int main (int argc, char *argv[])
+{
+	int a, b = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (a = 1; a < argc; a++)
+	{
+		if (_isdigit(*argv[a]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			b += atoi(argv[a]);
+	}
+	printf("%d\n", b);
+	return (0);
+}
