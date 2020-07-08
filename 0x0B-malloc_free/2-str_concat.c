071d08e072c0fbb6ea6bdc6eaa22e67e2a2ae454
didierrevelo <1829@holbertonschool.com>
2020-07-08 15:49:08 -0500

    mf

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 	int i, s, first, second;
 
 	if (s1 == NULL)
-		return (" ");
+		return ("");
 	if (s2 == NULL)
-		return (" ");
+		return ("");
 
 	for (first = 0; s1[first] != '\0'; first++)
 	{
