bca0a75a1822431b2f0331446243ba1966315e7f
didierrevelo <1829@holbertonschool.com>
2020-07-07 22:18:31 -0500

    mf

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,15 +10,11 @@ char *_strdup(char *str)
 	int i, s = 0;
 	char *a = malloc(sizeof(char) * s + 1);
 
-//	if (str == NULL || a == NULL)
-//		return (NULL);
+	if (str == NULL || a == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str == NULL || a == NULL)
-			return (NULL);
-		else
-			a[i] = str[i];
-	}
+		a[i] = str[i];
+	
 	return (a);
 	free(a);
 }
