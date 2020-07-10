30415633b8b603694fd94d5eaa94edcb43ba97ed
didierrevelo <1829@holbertonschool.com>
2020-07-09 22:41:04 -0500

    mmf

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
 	for (b = 0; b < nmemb * size; b++)
 		p[b] = 0;
 	return (p);
