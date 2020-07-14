ddd6940178a55f7ae0508f8084e171aed85f41fd
didierrevelo <1829@holbertonschool.com>
2020-07-14 03:13:04 -0500

    structtype

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ *print_dog - this function print struct dog
+ *@d: pointer of dog
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
