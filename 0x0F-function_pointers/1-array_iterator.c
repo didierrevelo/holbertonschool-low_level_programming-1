630edc5fad8905001c97b1ff512298ae958d7ed4
didierrevelo <1829@holbertonschool.com>
2020-07-16 09:14:42 -0500

    fp

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,10 @@
 #include "function_pointers.h"
 /**
  *array_iterator - this function
+ *@array: array of values
+ *@size: size of array
+ *@action: pointer of action in the array
+ *Return: nothing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
