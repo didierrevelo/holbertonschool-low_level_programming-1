e20528ced60bd0f7a36bee872ca16a223a28e657
didierrevelo <1829@holbertonschool.com>
2020-07-17 01:30:17 -0500

    functpoin

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s)
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
+	printf("Error\n");
+	exit(99);
 }
