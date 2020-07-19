59cd89e943dd35cfafbe1c10a4fbe0f7ae0b4aca
didierrevelo <1829@holbertonschool.com>
2020-07-19 09:44:25 -0500

    asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
 
-    message db "Hello, world!", 10
+    message db "Hello, Holberton", 10
 
 section .text
 
@@ -10,5 +10,5 @@ main:
     mov rax, 1
     mov rdi, 1
     mov rsi, message
-    mov rdx, 14
+    mov rdx, 17
     syscall
