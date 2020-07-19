5a3774e1c3733c39807553b799287882b37ad3d2
didierrevelo <1829@holbertonschool.com>
2020-07-19 09:10:56 -0500

    variafun

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,5 +15,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void prin_c(va_list ch);
+void prin_i(va_list in);
+void prin_f(va_list fl);
+void prin_s(va_list str);
 
 #endif /* VARIADIC_FUNCTIONS_H */
