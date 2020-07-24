0a91742d54cc73b7dd271020011942061785dca0
didierrevelo <1829@holbertonschool.com>
2020-07-23 22:42:10 -0500

    link

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,26 +7,25 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    int c = 0;
-    if (head == NULL)
-    return (NULL);
+	list_t *new;
+	int c = 0;
 
-    new = (list_t *) malloc(sizeof(list_t));
-    if (new == NULL)
-    return (NULL);
-
-    if (*head == NULL)
-    new->next = NULL;
-    else
-        new->next = *head;
-        new->str = strdup(str);
-        if (str)
-        {
-        while (str[c] != '\0')
-		c++;
-        }
-        new->len = c;
-        *head = new;
-        return (*head); 
+	if (head == NULL)
+		return (NULL);
+	new = (list_t *) malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+		new->next = NULL;
+	else
+		new->next = *head;
+	new->str = strdup(str);
+	if (str)
+	{
+		while (str[c] != '\0')
+			c++;
+	}
+	new->len = c;
+	*head = new;
+	return (*head);
 }
