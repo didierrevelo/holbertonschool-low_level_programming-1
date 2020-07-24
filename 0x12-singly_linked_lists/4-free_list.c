a4fd35027310c73105b65ccadd3081286cb2de9a
didierrevelo <1829@holbertonschool.com>
2020-07-23 23:39:08 -0500

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - this function free a list_t list
+ * @head: values
+ */
+void free_list(list_t *head)
+{
+    list_t *tem;
+    if (head == NULL)
+    return;
+    while (head->next != NULL)
+    {
+        tem = head->next;
+        free(head->str);
+        free(head);
+        head = tem;
+    }
+    free(head->str);
+    free(head);
+}
