3f31fd95e9dd7cd4176f42ce0b41378f12c18158
didierrevelo <1829@holbertonschool.com>
2020-07-23 23:41:06 -0500

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,16 +5,16 @@
  */
 void free_list(list_t *head)
 {
-    list_t *tem;
-    if (head == NULL)
-    return;
-    while (head->next != NULL)
-    {
-        tem = head->next;
-        free(head->str);
-        free(head);
-        head = tem;
-    }
-    free(head->str);
-    free(head);
+	list_t *tem;
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		tem = head->next;
+		free(head->str);
+		free(head);
+		head = tem;
+	}
+	free(head->str);
+	free(head);
 }
