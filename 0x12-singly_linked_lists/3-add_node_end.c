be377f8820ee7e445d59037809dfa7f0779be515
didierrevelo <1829@holbertonschool.com>
2020-07-23 23:03:56 -0500

    add

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_node_end - this function add a new node to list
+ * @head: pointer to the head of th list
+ * @str: string to print
+ * Return: addres of the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tem = *head;
+	int c = 0;
+	new = (list_t *) malloc(sizeof(list_t));
+	if (new)
+	{
+		new->str = strdup(str);
+		if (str)
+		{
+			while (new->str[c])
+				c++;
+		}
+		new->len = c;
+		if (!*head)
+		{
+			*head = new;
+			return (*head);
+		}
+		while (tem->next)
+			tem = tem->next;
+		tem->next = new;
+		return (new);
+	}
+	return (NULL);
+}
