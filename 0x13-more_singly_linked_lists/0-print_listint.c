14f171ca3266b7b194f8b231b1da6a9485667f54
didierrevelo <1829@holbertonschool.com>
2020-07-28 13:47:36 -0500

    The function that prints integers is added and returns the number of nodes.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * size_t print_listint - this function prints all the elements of a listint_t list.
+ * @h: pointer list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int runcount;
+
+	if (h == NULL)
+	return (0);
+
+	for (runcount = 0; h != NULL; runcount++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (runcount);
+}
