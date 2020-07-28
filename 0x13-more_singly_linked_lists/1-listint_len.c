1fdb3d90f9a7f55f3aa39ac7a23f9f9c8eb596ea
didierrevelo <1829@holbertonschool.com>
2020-07-28 14:34:00 -0500

    function that print the number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - this function return the numbers of elements of a list.
+ * @h: it´s the values to prints
+ * Return: the number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int runcount = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+		h = h->next;
+		runcount++;
+	return (runcount);
+}
