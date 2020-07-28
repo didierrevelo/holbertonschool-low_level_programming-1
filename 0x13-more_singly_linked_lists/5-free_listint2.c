dc0136b77dce0e5fdf30067267a16535dd72dfa0
didierrevelo <1829@holbertonschool.com>
2020-07-28 17:44:57 -0500

    add function frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint - this function free a list_t list
+ * @head: values
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tem;
+
+	if (head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		tem = (*head)->next;
+		free(*head);
+		*head = tem;
+	}
+	free(*head);
+	*head = NULL;
+}
