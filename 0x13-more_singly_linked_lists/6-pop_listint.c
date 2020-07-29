d65f2ae9a15a4e80b3c584d45e1adb0d675c284f
didierrevelo <1829@holbertonschool.com>
2020-07-28 19:25:01 -0500

    corrections

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,10 +9,11 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int n = (*head)->n;
+	int n;
 
 	if (head == NULL || *head == NULL)
 	return (0);
+	n = (*head)->n;
 	temp = *head;
 	*head = (*head)->next;
 	free(temp);
