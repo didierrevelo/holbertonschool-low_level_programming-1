760b868f809b1e6adbd7dd217e2e252effc664bb
didierrevelo <1829@holbertonschool.com>
2020-07-31 12:30:54 -0500

    betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,15 +7,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index == 64)
+	if (index >= 64)
 	return (-1);
 
 	if ((n >> index) & 1)
 	{
 		return (1);
 	}
-	else 
-	{
 	return (0);
-	}
 }
