5f79de60092d07c8d15d45154847cceff3e5dffa
didierrevelo <1829@holbertonschool.com>
2020-07-31 09:56:18 -0500

    betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 /**
+ * print_binary - print the binary representaion
+ * @n: number to represented
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-    {
-        print_binary(n >> 1);
-        _putchar ((n & 1) + '0');
-    }
-    else
-    {
-       _putchar (n + '0');
-    }
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+		_putchar ((n & 1) + '0');
+	}
+	else
+	{
+		_putchar (n + '0');
+	}
 }
