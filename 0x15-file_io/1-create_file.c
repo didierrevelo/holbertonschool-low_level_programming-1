0a1eb73a20aee239cc479f4768edefdf3e7b6499
didierrevelo <1829@holbertonschool.com>
2020-08-09 23:04:11 -0500

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		for (runner = 0; text_content[runner] != '\0'; runner++)
