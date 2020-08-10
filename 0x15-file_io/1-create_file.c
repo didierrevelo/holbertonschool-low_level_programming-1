76afdff62ef181134f7ffcea40a88b69ca5be91e
didierrevelo <1829@holbertonschool.com>
2020-08-09 22:45:07 -0500

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * create_file - this function create a file void
- * @filename: file name.
+ * @filename: file name
  * @text_content: content of the file
  * Return: 1 on success, -1 on failure
  * (file can not be created, file
@@ -13,20 +13,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	return (-1);
-	if (text_content == NULL)
-	return (-1);
-
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-	if (fd < 0)
-	{
-		if (errno == EEXIST)
-		{
-			fd = open(filename, O_WRONLY | O_TRUNC);
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY | O_TRUNC, 0600);
 			if (fd == -1)
 			return (-1);
-		}
-		return (-1);
-	}
 
 	if (text_content)
 	{
