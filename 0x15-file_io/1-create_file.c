f6586d31730143aba4076a148b95a33f66abd442
didierrevelo <1829@holbertonschool.com>
2020-08-09 22:34:12 -0500

    write a function that create a file void

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * create_file - this function create a file void
+ * @filename: file name.
+ * @text_content: content of the file
+ * Return: 1 on success, -1 on failure
+ * (file can not be created, file
+ * can not be written, write “fails”, etc…)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, runner = 0;
+
+	if (filename == NULL)
+	return (-1);
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	if (fd < 0)
+	{
+		if (errno == EEXIST)
+		{
+			fd = open(filename, O_WRONLY | O_TRUNC);
+			if (fd == -1)
+			return (-1);
+		}
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		while (text_content[runner] != '\0')
+		runner++;
+		write(fd, text_content, runner);
+	}
+
+
+close(fd);
+return (1);
+}
